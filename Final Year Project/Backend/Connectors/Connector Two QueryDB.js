0c392028cc26e667830a5e4224258e0a9cd6dcda
Owen Hayden <oh215@kent.ac.uk>
2026-05-05 11:45:51 +0100

    New Connector methods and Node.js update.

--- Final Year Project/Backend/Connectors/Connector Two QueryDB.js
+++ Final Year Project/Backend/Connectors/Connector Two QueryDB.js
@@ -1,10 +1,9 @@
-// Connector method shell one. //
 let mysql = require('mysql');
 
 //Creation of the connection using host address, //
 // User ,and Pass //
 let con = mysql.createConnection({
-    host: "",
+    host: "dragon.kent.ac.uk",
     user: "oh215",
     password: "Enter_on_Use",
     database: "oh215"
@@ -13,12 +12,18 @@ let con = mysql.createConnection({
 //Error throw and connection confirm//
 con.connect(function (err){
     if (err) throw err;
-    console.log("Connected");
-
     // Table queries for required connection here //
+    con.query("SELECT * FROM Meals", function (err, result){
+        if (err) throw err;
+        console.log(result);
+    });
     // Query such as request data or update tables //
-
+    let adr = 'Mountain 21';
+    let sql = 'SELECT * FROM customers WHERE address = ' + mysql.escape(adr);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
     // Connection close //
     con.end();
 });
-
