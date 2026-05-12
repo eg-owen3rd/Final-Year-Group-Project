f83eb18b9e8dc065141de93af5de231630ad9edf
Jhesy Cueva <258678827+Latin7@users.noreply.github.com>
2026-05-12 23:19:39 +0100

    Added frontend input for meal forms

--- Final Year Project/Frontend/Application/js/app.js
+++ Final Year Project/Frontend/Application/js/app.js
@@ -1,1 +1,22 @@
-console.log("Testing JS file");
+const button = document.querySelector(".meal-button");
+
+if (button) {
+    button.addEventListener("click", function () {
+
+    // Get inputs
+    const ingredients =
+    document.getElementById("ingredients").value;
+
+    const otherEquipment =
+    document.getElementById("other-equipment").value;
+
+    const selectedSkill =
+    document.querySelector("input[name='skill']:checked")
+    .parentElement.textContent.trim();
+
+    // Print values
+    console.log("Ingredients:", ingredients);
+    console.log("Other Equipment:", otherEquipment);
+    console.log("Skill Level:", selectedSkill);
+});
+}
